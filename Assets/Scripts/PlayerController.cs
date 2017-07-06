d378ea78ff75c7e39ef672a6a188fa962be66617
Bradley Court <bradleycourt@hotmail.com>
2017-07-06 10:28:46 +0930

    modified script name to not conflict with the character controller class, fixed gravity issues, jumping looks pretty good

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 5;
+    public float jumpVel = 8.0F;
+    public CharacterController cc;
+ //   private Vector3 moveDirection = Vector3.zero;
+
+    public Vector3 moveDirection;
+
+    // Use this for initialization
+    void Start ()
+    {
+       cc = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal")*speed, moveDirection.y, Input.GetAxisRaw("Vertical")*speed);
+
+
+        // Gravity at current does not work!  
+        if (cc.isGrounded)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpVel;
+            }
+        }
+        else
+        {
+            moveDirection += Physics.gravity * Time.deltaTime;
+        }
+
+        cc.Move(moveDirection* Time.deltaTime);
+    }
+   
+}
