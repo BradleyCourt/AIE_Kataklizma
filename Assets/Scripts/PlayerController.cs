cd3b272ee4dc5c6c6626ffb16125857e35a98d8f
Bradley Court <bradleycourt@hotmail.com>
2017-07-06 14:40:49 +0930

    implemented incontrol, added an enemy, added collider and check for first attack, added health and the ability to be damages, made a sword for testing the collider (useless now)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,41 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public int speed = 5;
     public float jumpVel = 8.0F;
     public CharacterController cc;
- //   private Vector3 moveDirection = Vector3.zero;
+    public GameObject Player;
+    Animator animate;
+    //   private Vector3 moveDirection = Vector3.zero;
 
-    public Vector3 moveDirection;
+    private Vector3 moveDirection;
+
+    BoxCollider attackZone;
 
     // Use this for initialization
-    void Start ()
-    {
-       cc = GetComponent<CharacterController>();
+    void Start() {
+        cc = GetComponent<CharacterController>();
+        animate = GetComponent<Animator>();
     }
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal")*speed, moveDirection.y, Input.GetAxisRaw("Vertical")*speed);
+
+    // Update is called once per frame
+    void Update() {
+
+
+
+        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
 
 
         // Gravity at current does not work!  
-        if (cc.isGrounded)
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
+        if (cc.isGrounded) {
+
+            if (Input.GetButtonDown("Jump")) {
                 moveDirection.y = jumpVel;
             }
-        }
-        else
-        {
+        } else {
+
             moveDirection += Physics.gravity * Time.deltaTime;
         }
 
-        cc.Move(moveDirection* Time.deltaTime);
+        cc.Move(moveDirection * Time.deltaTime);
+
+        //if (Input.GetMouseButtonDown(0)) {
+        //    if (!animate.IsInTransition(0)) {
+        //        animate.SetBool("Attacking", true);
+        //        Debug.Log("Hah!");
+        //    }
+        //}
     }
-   
+
+    void Attack() {
+        Collider[] targets = Physics.OverlapBox(attackZone.bounds.center + transform.position /* ?? */, attackZone.bounds.extents, transform.rotation);
+        foreach (Collider target in targets) {
+            var playerStats = target.GetComponent<playerStats>();
+            if (playerStats) {
+                // Apply Damage
+            }
+
+        }
+
+    }
+
 }
---
+++ Assets/Scripts/playerStats.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class playerStats : MonoBehaviour
+{
+    public int MaxHealth;
+
+    public int _CurrentHealth;
+    public int CurrentHealth {
+        get { return _CurrentHealth; }
+        set {
+
+            _CurrentHealth = Mathf.Max(value, 0);
+
+            if (_CurrentHealth <= 0)
+                BroadcastMessage("OnObjectDeath", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    public bool IsAlive {  get { return _CurrentHealth > 0; } }
+
+
+	// Use this for initialization
+	void Start ()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void OnObjectDeath() {
+        // Do Soldier-of-Fortune Death Sequence
+        Destroy(gameObject);
+    }
+}
+
