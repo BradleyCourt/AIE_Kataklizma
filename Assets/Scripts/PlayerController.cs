f6fa979c086ca3a17cc223cd99431677e63033ed
Bradley Court <bradleycourt@hotmail.com>
2017-07-07 11:58:13 +0930

    removed incontrol, added controller support, added tags and layers, updated player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,49 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
     public int speed = 5;
-    public int RunSpeed = 10;
     public int Charge = 50; // not implemented yet, intention is once used movement is limited and you charge for X seconds or until collision with something
     public float JumpVel = 8.0F;
     public int TempDamage = 0;
     public CharacterController cc;
     public GameObject Player;
+    public bool isCharging = false;
     //   private Vector3 moveDirection = Vector3.zero;
 
     private Vector3 moveDirection;
 
-    BoxCollider attackZone;
+    public BoxCollider attackZone;
 
     // Use this for initialization
-    void Start() {
+    void Start()
+    {
         cc = GetComponent<CharacterController>();
     }
-
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+        if (isCharging == false)
+        {
+             moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
+        }
 
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal") * RunSpeed, moveDirection.y, Input.GetAxis("Vertical") * RunSpeed);
-        }
-        else
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
-        }
 
 
         if (Input.GetMouseButtonDown(0))
             Attack();
 
-
         // Gravity at current does not work!  
-        if (cc.isGrounded) {
+        if (cc.isGrounded)
+        {
 
-            if (Input.GetButtonDown("Jump")) {
+            if (Input.GetButtonDown("Jump"))
+            {
                 moveDirection.y = JumpVel;
             }
-        } else {
+        }
+        else
+        {
 
             moveDirection += Physics.gravity * Time.deltaTime;
         }
@@ -52,11 +53,22 @@ public class PlayerController : MonoBehaviour {
         cc.Move(moveDirection * Time.deltaTime);
     }
 
-    void Attack() {
-        Collider[] targets = Physics.OverlapBox(attackZone.bounds.center + transform.position /* ?? */, attackZone.bounds.extents, transform.rotation); // this line is causing a null reference
-        foreach (Collider target in targets) {
+    void Attack()
+    {
+
+        var centreOffset = transform.localToWorldMatrix.MultiplyVector(attackZone.center);
+        var centre = transform.position + centreOffset;
+        var size = transform.localToWorldMatrix.MultiplyVector(attackZone.size);
+
+        Collider[] targets = Physics.OverlapBox(centre, size, new Quaternion(), ~8);
+
+        foreach (Collider target in targets)
+        {
+            if (target.gameObject.tag == "Player") continue;
+
             var playerStats = target.GetComponent<playerStats>();
-            if (playerStats) {
+            if (playerStats)
+            {
                 playerStats.RemoveHealth(TempDamage);
             }
 
