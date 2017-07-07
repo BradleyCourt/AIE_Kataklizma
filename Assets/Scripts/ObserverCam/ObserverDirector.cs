be4b76986530cd89df9a1f98c5ba92ad786749ea
Immersive <immrsv@users.noreply.github.com>
2017-07-07 13:41:33 +0930

    Added placeholders for script hierarchy

---
+++ Assets/Scripts/ObserverCam/ObserverDirector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObserverDirector : MonoBehaviour {
+
+    public GameObject Target;
+    public ObserverController SelectedObserver;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var view = new Vector3();
+        view.x = Input.GetAxis("CameraHorizontal");
+        view.y = Input.GetAxis("CameraVertical");
+        view.z = Input.GetAxis("CameraZoom");
+
+        var currentOffset = SelectedObserver.transform.position - Target.transform.position;
+
+	}
+}
--- Assets/Scripts/ObserverCam/StableCam.cs
+++ Assets/Scripts/ObserverCam/StableCam.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraDirector : MonoBehaviour {
+public class StableCam : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
