addfe204c0bf3b222ae8e6cb6a80255c60438e19
Bradley Court <bradleycourt@hotmail.com>
2017-07-07 10:26:22 +0930

    added run, added damage and the ability to attack, null pointer currently exists, begun implementing incontrol, removed initialisation of the animator

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public int speed = 5;
-    public float jumpVel = 8.0F;
+    public int RunSpeed = 10;
+    public int Charge = 50; // not implemented yet, intention is once used movement is limited and you charge for X seconds or until collision with something
+    public float JumpVel = 8.0F;
+    public int TempDamage = 0;
     public CharacterController cc;
     public GameObject Player;
-    Animator animate;
     //   private Vector3 moveDirection = Vector3.zero;
 
     private Vector3 moveDirection;
@@ -17,22 +19,30 @@ public class PlayerController : MonoBehaviour {
     // Use this for initialization
     void Start() {
         cc = GetComponent<CharacterController>();
-        animate = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * RunSpeed, moveDirection.y, Input.GetAxis("Vertical") * RunSpeed);
+        }
+        else
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
+        }
 
 
-        moveDirection = new Vector3(Input.GetAxis("Horizontal") * speed, moveDirection.y, Input.GetAxis("Vertical") * speed);
+        if (Input.GetMouseButtonDown(0))
+            Attack();
 
 
         // Gravity at current does not work!  
         if (cc.isGrounded) {
 
             if (Input.GetButtonDown("Jump")) {
-                moveDirection.y = jumpVel;
+                moveDirection.y = JumpVel;
             }
         } else {
 
@@ -43,11 +53,11 @@ public class PlayerController : MonoBehaviour {
     }
 
     void Attack() {
-        Collider[] targets = Physics.OverlapBox(attackZone.bounds.center + transform.position /* ?? */, attackZone.bounds.extents, transform.rotation);
+        Collider[] targets = Physics.OverlapBox(attackZone.bounds.center + transform.position /* ?? */, attackZone.bounds.extents, transform.rotation); // this line is causing a null reference
         foreach (Collider target in targets) {
             var playerStats = target.GetComponent<playerStats>();
             if (playerStats) {
-                // Apply Damage
+                playerStats.RemoveHealth(TempDamage);
             }
 
         }
