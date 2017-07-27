00b0c5c24f654751816dd248a544accbe97011a3
Immersive <immrsv@users.noreply.github.com>
2017-07-27 13:44:57 +0930

    Broke camera, fixed "Missing Namespace" error.
    Signed-off-by: Immersive <immrsv@users.noreply.github.com>

--- Assets/Scripts/ObserverCam/ObserverController.cs
+++ Assets/Scripts/ObserverCam/ObserverController.cs
@@ -13,6 +13,12 @@ public class ObserverController : MonoBehaviour {
 
     private Vector3 Offset;
 
+    [HideInInspector]
+    public float PositionTheta = 0; // "Yaw"
+
+    [HideInInspector]
+    public float PositionPhi = 20; // "Pitch"
+
 	// Use this for initialization
 	void Start () {
 		
@@ -21,28 +27,6 @@ public class ObserverController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-
-        //var currentDirn = ObserverCam.transform.forward;
-        //var targetDirn = (Target.transform.position - transform.position).normalized;
-
-        //var flatCurrentDirn = new Vector3(currentDirn.x, 0, currentDirn.z).normalized; // Vector component locked to XZ plane
-        //var flatTargetDirn = new Vector3(targetDirn.x, 0, targetDirn.z).normalized; // Vector component locked to XZ plane
-
-        //var yawDirn = Mathf.Asin(Vector3.Dot(flatCurrentDirn, flatTargetDirn)).CompareTo(0.0f);
-        //var yawDelta = Mathf.Acos(Vector3.Dot(flatCurrentDirn, flatTargetDirn)) * Mathf.Rad2Deg * yawDirn;
-
-        //var currentPitch = Mathf.Acos(Vector3.Dot(flatCurrentDirn, currentDirn)) * Mathf.Rad2Deg;
-        //var targetPitch = Mathf.Acos(Vector3.Dot(flatTargetDirn, targetDirn)) * Mathf.Rad2Deg;
-
-        //var pitchDelta = targetPitch - currentPitch;
-
-        //if (Mathf.Abs(yawDelta) >= 0.1f)
-        //    ObserverCam.transform.Rotate(0, yawDelta, 0, Space.World);
-
-        //if (Mathf.Abs(pitchDelta) >= 0.1f)
-        //    ObserverCam.transform.Rotate(pitchDelta, 0, 0, Space.Self);
-
         ObserverCam.transform.LookAt(Target.transform);
-
     }
 }
--- Assets/Scripts/ObserverCam/ObserverDirector.cs
+++ Assets/Scripts/ObserverCam/ObserverDirector.cs
@@ -41,45 +41,20 @@ public class ObserverDirector : MonoBehaviour {
 
 
         if (Cursor.lockState == CursorLockMode.Locked) {
-            var view = new Vector3();
-            view.x = Input.GetAxis("ViewHorizontal");
-            view.y = Input.GetAxis("ViewVertical");
 
-            var currentOffset = Selected.Observer.transform.position - Target.transform.position;
-            var distance = currentOffset.magnitude;
-            var direction = currentOffset.normalized;
-            var track = new Vector3(direction.x, 0, direction.z).normalized; // Direction along ground plane
-
-            // Yaw Delta
-            var yawDelta = Quaternion.Euler(new Vector3(0, Input.GetAxis("ViewHorizontal"), 0));
-
-            // Pitch Delta (Clamped)
-            var currentPitch = 90.0f - (Mathf.Asin(Vector3.Dot(direction, track)) * Mathf.Rad2Deg);
-            var pitchDeltaMin = Selected.MinPitch - currentPitch;
-            var pitchDeltaMax = Selected.MaxPitch - currentPitch;
-            var pitchDelta = Quaternion.Euler(new Vector3(Mathf.Clamp(Input.GetAxis("ViewVertical"), pitchDeltaMin, pitchDeltaMax), 0, 0));
-
-            Debug.Log("Current Pitch: [" 
-                + Selected.MinPitch.ToString("N2") + ":" + pitchDeltaMin.ToString("N2") + "] < "
-                + currentPitch.ToString("N2") + " < [" 
-                + Selected.MaxPitch.ToString("N2") + ":" + pitchDeltaMax.ToString("N2") + "]");
-
-            var xform = Matrix4x4.identity;
-            
-
-            // Zoom Delta
-            var zoom = distance; // * Mathf.Clamp((1.0f + Input.GetAxis("CameraZoom")), 0.8f, 1.2f);
+            Selected.Observer.PositionTheta += Input.GetAxis("ViewHorizontal");
+            Selected.Observer.PositionPhi = Mathf.Clamp(Selected.Observer.PositionPhi + Input.GetAxis("ViewVertical"), Selected.MinPitch, Selected.MaxPitch);
 
             //if (distance <= 1.0f)
             //    zoom = Mathf.Max(1.0f, zoom);
 
-            var offset = direction;
-            offset = yawDelta * offset;
-            offset = pitchDelta * offset;
-            offset = zoom * offset;
+            //var offset = direction;
+            //offset = yawDelta * offset;
+            //offset = pitchDelta * offset;
+            //offset = zoom * offset;
 
             // Rotate camera around target
-            Selected.Observer.transform.position = (Target.transform.position + offset);
+            //Selected.Observer.transform.position = (Target.transform.position + offset);
 
         }
 
