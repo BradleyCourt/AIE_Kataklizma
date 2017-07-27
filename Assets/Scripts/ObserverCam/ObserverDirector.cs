f8bb4c89b444ae7a58d7bfe5d548d4fe06c53605
Immersive <immrsv@users.noreply.github.com>
2017-07-27 15:09:14 +0930

    Added Motion to ObserverDirector.  Check Trello for how to use.
    Signed-off-by: Immersive <immrsv@users.noreply.github.com>

--- Assets/Scripts/ObserverCam/ObserverDirector.cs
+++ Assets/Scripts/ObserverCam/ObserverDirector.cs
@@ -12,7 +12,9 @@ public class ObserverDirector : MonoBehaviour {
     }
 
     public GameObject Target;
+
     protected ObserverDefinition Selected;
+    protected Rigidbody TargetRb;
 
     public List<ObserverDefinition> Observers;
     
@@ -23,12 +25,23 @@ public class ObserverDirector : MonoBehaviour {
 
         Selected = Observers[Random.Range(0, Observers.Count)];
 
+        TargetRb = Target.GetComponent<Rigidbody>();
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        UpdateInputState();
+        UpdateCamera();
+        UpdateMotion();
+
+    }
+
+    /// <summary>
+    /// Capture or Release mouse cursor
+    /// </summary>
+    void UpdateInputState() {
         if (Input.GetMouseButtonDown(2)) { // Middle Mouse toggles mouse capture
             if (Cursor.lockState != CursorLockMode.Locked) {
                 Cursor.lockState = CursorLockMode.Locked;
@@ -38,26 +51,54 @@ public class ObserverDirector : MonoBehaviour {
                 Cursor.visible = true;
             }
         }
+    }
 
 
+    /// <summary>
+    /// Apply captured mouse motion to camera.  Update camera position.
+    /// </summary>
+    void UpdateCamera() {
         if (Cursor.lockState == CursorLockMode.Locked) {
 
-            Selected.Observer.PositionTheta += Input.GetAxis("ViewHorizontal");
+            // Update Camera
+            Selected.Observer.PositionTheta = Mathf.Repeat(Selected.Observer.PositionTheta + Input.GetAxis("ViewHorizontal"), 360);
             Selected.Observer.PositionPhi = Mathf.Clamp(Selected.Observer.PositionPhi + Input.GetAxis("ViewVertical"), Selected.MinPitch, Selected.MaxPitch);
-            
-            var yaw = Quaternion.Euler(0, Selected.Observer.PositionTheta, 0);
-            var pitch = Quaternion.Euler(-Selected.Observer.PositionPhi, 0, 0);
 
-
-            var offset = Vector3.forward * Selected.Observer.Distance;
-
-            offset = pitch * offset;
-            offset = yaw * offset;
-
-            // Rotate camera around target
-            Selected.Observer.transform.position = (Target.transform.position + offset);
-
-            Debug.Log("View From: " + Selected.Observer.PositionTheta.ToString("N2") + " Theta, " + Selected.Observer.PositionPhi.ToString("N2") + " Phi.");
         }
+
+
+        // Apply Camera
+        var yaw = Quaternion.Euler(0, Selected.Observer.PositionTheta, 0);
+        var pitch = Quaternion.Euler(-Selected.Observer.PositionPhi, 0, 0);
+
+        var offset = Vector3.forward * Selected.Observer.Distance;
+
+        offset = pitch * offset;
+        offset = yaw * offset;
+
+        // Rotate camera around target
+        Selected.Observer.transform.position = (Target.transform.position + offset);
+
+        Debug.Log("View From: " + Selected.Observer.PositionTheta.ToString("N2") + " Theta, " + Selected.Observer.PositionPhi.ToString("N2") + " Phi.");
+    }
+
+
+    /// <summary>
+    /// Apply captured motion to character.  Update character velocity.
+    /// </summary>
+    void UpdateMotion() {
+
+        var cameraDirection = Target.transform.position - Selected.Observer.transform.position;
+
+        var characterFwd = new Vector3(cameraDirection.x, 0, cameraDirection.z).normalized;
+        var characterRight = new Vector3(cameraDirection.z, 0, -cameraDirection.x).normalized;
+
+
+        var motion = characterFwd * Input.GetAxis("MoveVertical") + characterRight * Input.GetAxis("MoveHorizontal");
+        motion.Normalize();
+
+        Target.transform.LookAt(Target.transform.position + motion);
+
+        TargetRb.velocity = motion * 5.0f;
     }
 }
