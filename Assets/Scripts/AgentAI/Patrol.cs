b2f803b5b75d0113b89b44b78ce46178621ff1fa
Bradley Court <bradleycourt@hotmail.com>
2017-07-28 10:17:22 +0930

    Making the AI stop within a certain dist of the player (in progress)

--- Assets/Scripts/AgentAI/Patrol.cs
+++ Assets/Scripts/AgentAI/Patrol.cs
@@ -9,10 +9,11 @@ public class Patrol : MonoBehaviour
     public Transform[] points;
     private int destPoint = 0;
     private NavMeshAgent agent;
-
-
+    private GameObject player;
+    private bool isInRange;
     void Start()
     {
+        bool isInRange = false;
         agent = GetComponent<NavMeshAgent>();
 
         // Disabling auto-braking allows for continuous movement
@@ -20,7 +21,10 @@ public class Patrol : MonoBehaviour
         // approaches a destination point).
         agent.autoBraking = false;
 
-        GotoNextPoint();
+        //GotoNextPoint();
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        agent.SetDestination(player.transform.position);
     }
 
 
@@ -49,7 +53,16 @@ public class Patrol : MonoBehaviour
     {
         // Choose the next destination point when the agent gets
         // close to the current one.
-        if (!agent.pathPending && agent.remainingDistance < 0.5f)
-            GotoNextPoint();
+        if (!agent.pathPending && agent.remainingDistance > 5)// && isInRange != true)
+            //GotoNextPoint();
+            agent.SetDestination(player.transform.position);
+        else if (!agent.pathPending && agent.remainingDistance < 5)// && isInRange != true)
+        {
+           //isInRange = true;
+            agent.SetDestination(agent.transform.position);
+            Debug.Log("stopmoving");
+        }
+
+
     }
 }
