6f9e72a5a7e2739bcf6747c9b23be5a2ba46a704
Immersive <immrsv@users.noreply.github.com>
2017-08-17 11:21:05 +0930

    Added MapProcGen framework.  Added Architecture diagrams.
    Signed-off-by: Immersive <immrsv@users.noreply.github.com>

---
+++ Assets/Scripts/MapGen/MapGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MapGen {
+    public class MapGenerator : MonoBehaviour {
+
+        [Serializable]
+        public enum TileType {
+            Tile5m,
+            Tile10m,
+            Tile20m,
+            Tile30m,
+            Tile40m,
+            TileRoad
+        }
+
+        [Serializable]
+        public class SourceTilePreset {
+            public TileType Type;
+            public List<MapTile> Tiles;
+
+        }
+
+        public List<SourceTilePreset> SourceTilePresets;
+
+
+    }
+}
+
---
+++ Assets/Scripts/MapGen/MapTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MapGen {
+    [System.Serializable]
+    public class MapTile {
+
+        [System.Serializable]
+        [System.Flags]
+        public enum RoadConnections {
+            None = 0,
+            Front = 1,
+            Back = 2,
+            Left = 4,
+            Right = 8,
+        }
+        
+        [Range(0,1)]
+        public float Weight;
+        public GameObject TilePrefab;
+        
+    }
+}
