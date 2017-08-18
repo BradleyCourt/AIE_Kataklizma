5003fabeac42ebb8ec0d022005254503e2aa3541
Immersive <immrsv@users.noreply.github.com>
2017-08-18 15:44:39 +0930

    Added TileConnection to Road tiles ready for Map ProcGen.
    Signed-off-by: Immersive <immrsv@users.noreply.github.com>

---
+++ Assets/Scripts/MapGen/TileConnection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace MapGen {
+    public class TileConnection : MonoBehaviour {
+
+        [Tooltip("Positive Z")]
+        public bool Forward;
+
+        [Tooltip("Negative Z")]
+        public bool Back;
+
+        [Tooltip("Negative X")]
+        public bool Left;
+
+        [Tooltip("Negative X")]
+        public bool Right;
+    }
+}
