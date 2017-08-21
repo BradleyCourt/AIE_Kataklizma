6774be4739143f9ff5235ccd46628a2273cb06de
Riley Huppatz <riley5694@gmail.com>
2017-08-21 21:16:04 +0930

    Messing around with title screen ideas

---
+++ Assets/Scripts/Canvas/ActiveGame/Marquee.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marquee : MonoBehaviour {
+    public string message = "STATE OF EMERGANCY";
+    public float scrollSpeed;
+    public Rect messageRect;
+
+    void OnGUI()
+    {
+        //If there is no set Rect
+        if(messageRect.width == 0)
+        {
+            var dimensions = GUI.skin.label.CalcSize(new GUIContent(message));
+
+            messageRect.x = -dimensions.x;
+            messageRect.width = dimensions.x;
+            messageRect.height = dimensions.y;
+        }
+
+        messageRect.x += Time.deltaTime * scrollSpeed;
+
+        if(messageRect.x > Screen.width)
+        {
+            messageRect.x = -messageRect.width;
+        }
+
+        GUI.Label(messageRect, message);
+    }
+}
