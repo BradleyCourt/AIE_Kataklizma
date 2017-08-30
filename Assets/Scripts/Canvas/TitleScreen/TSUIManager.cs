434029a4ee4c40078c02fe810daf9a2efd1bd044
Riley Huppatz <riley5694@gmail.com>
2017-08-30 18:09:36 +0930

    Titlse screen testing

---
+++ Assets/Scripts/Canvas/TitleScreen/TSUIManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TSUIManager : MonoBehaviour
+{
+
+	private bool buttonClicked = false;
+
+	public VideoPlayer breakingNews;
+	public GameObject background;
+	public GameObject PAKPanel;
+	public VideoPlayer channelSurfing;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (buttonClicked == false && Input.anyKey)
+		{
+			
+			channelSurfing.Stop();
+			StartCoroutine("PlayVideo");
+			
+
+		}
+	}
+
+	IEnumerator PlayVideo()
+	{
+		breakingNews.Play();
+		print("should be playing");
+		buttonClicked = true;
+		PAKPanel.SetActive(false);
+		yield return new WaitForSeconds(3);
+		breakingNews.Stop();
+		background.SetActive(true);
+	}
+}
