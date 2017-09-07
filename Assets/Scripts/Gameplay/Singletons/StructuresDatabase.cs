a4b26828c30ea865ac8f3558f9aec4a1be517343
Immersive <immrsv@users.noreply.github.com>
2017-09-07 14:02:00 +0930

    Tinker, tinker, fiddle...

---
+++ Assets/Scripts/Gameplay/Singletons/StructuresDatabase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kataklizma.Gameplay.Singletons {
+    public class StructuresDatabase : MonoBehaviour {
+
+        protected static StructuresDatabase _Instance = null;
+        public static StructuresDatabase Instance {
+            get {
+                return _Instance;
+            }
+            set {
+                if (_Instance == null) _Instance = value;
+            }
+        }
+
+
+        void Awake() {
+
+        }
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
