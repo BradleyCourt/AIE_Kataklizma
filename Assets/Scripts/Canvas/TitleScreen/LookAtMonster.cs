c435b07b93a0466ea226d31230655c3540319b9d
Xaunaught <riley5694@gmail.com>
2017-09-20 16:48:37 +0930

    Title screen movers and camera

---
+++ Assets/Scripts/Canvas/TitleScreen/LookAtMonster.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAtMonster : MonoBehaviour {
+    public GameObject objectToLookAt;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(objectToLookAt.transform);
+    }
+}
