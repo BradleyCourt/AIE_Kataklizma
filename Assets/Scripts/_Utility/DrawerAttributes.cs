b70010793c27ce6c6ecf31b03723f65b451d9c49
Immersive <immrsv@users.noreply.github.com>
2017-09-27 21:54:56 +0930

    Restructured Editor scripts to fix Build issues.

---
+++ Assets/Scripts/_Utility/DrawerAttributes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BitfieldMaskAttribute : PropertyAttribute {
+    public int AllowedValues = ~0;
+    public bool ShowZeroValue = true;
+    public bool ShowCompoundValue = true;
+}
+
+public class UnityLayerAttribute : PropertyAttribute {
+
+}
+
+// This class defines the TagList attribute, so that
+// it can be used in your regular MonoBehaviour scripts:
+
+public class UnityTagAttribute : PropertyAttribute {
+
+}
