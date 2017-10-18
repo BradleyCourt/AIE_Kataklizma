e0a81bfadcd159542d3f4b6dcdf7f2f3c87c9ef1
Immersive <immrsv@users.noreply.github.com>
2017-10-18 15:40:55 +1030

    Added ConicZone and SpheroidZone, used to project Mutator AoE Effects.

---
+++ Assets/Scripts/Scriptables/ScriptedEffects/MovementEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Scriptables {
+
+    [CreateAssetMenu(fileName = "AudioEffect", menuName = "Kataklizma/Effects/Audio Effect")]
+    public class MovementEffect : AnchoredEffect {
+
+        public string AudioName;
+
+    }
+}
---
+++ Assets/Scripts/Scriptables/ScriptedZones/ConicZone.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Scriptables {
+
+    [CreateAssetMenu(fileName = "ConicZone", menuName = "Kataklizma/Zones/Conic Zone")]
+    public class ConicZone : ScriptedZone {
+
+        [Space]
+        public float NearRadius;
+        public float FarRadius;
+        public float Length;
+
+
+        protected bool OffsetsKnown = false;
+
+        protected Vector3 OriginDelta;
+        protected float SphereRadius;
+        protected float ConeHalfangle;
+        protected float ConeDotMinimum;
+
+        protected void FindOffsets() {
+            
+            ConeHalfangle = Mathf.Atan(FarRadius / Length);
+            ConeDotMinimum = Mathf.Sin(ConeHalfangle);
+            
+            OriginDelta = new Vector3();
+            OriginDelta.z = -Length * NearRadius / (FarRadius - NearRadius);
+
+            SphereRadius = new Vector2(Length + OriginDelta.z, FarRadius).magnitude;
+
+            OffsetsKnown = true;
+        }
+
+        
+
+        /// <summary>
+        /// Discover all Colliders within zone
+        /// </summary>
+        /// <param name="origin"></param>
+        /// <param name="layerMask">Default: Ignore "PlayerAvater" layers</param>
+        /// <returns></returns>
+        public override Collider[] OverlapZone(Transform origin, int layerMask = ~8) {
+
+            if (!OffsetsKnown) FindOffsets();
+
+            var derivedOrigin = origin.position + origin.TransformVector(OriginDelta);
+
+            var range = Physics.OverlapSphere(derivedOrigin, SphereRadius, layerMask);
+
+            var results = new List<Collider>();
+
+            foreach (var collider in range) {
+                if (collider == null) continue;
+                if (!CanAffectTags.Contains(collider.gameObject.tag)) continue;
+
+                // Check collider is within angle and min range
+                var direction = (collider.transform.position - derivedOrigin).normalized;
+                var dot = Vector3.Dot(direction, origin.forward);
+
+                if (dot < ConeDotMinimum || direction.magnitude < OriginDelta.z) continue;
+
+                // Add to list
+                results.Add(collider);
+            }
+
+            return results.ToArray();
+        }
+    }
+}
