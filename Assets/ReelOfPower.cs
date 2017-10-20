846578cda07df17208743fd6589ca3774ae6e2cd
AIE\s171*** <talscartalcom@gmail.com>
2017-10-20 15:46:34 +1030

    no message

--- Assets/ReelOfPower.cs
+++ Assets/ReelOfPower.cs
@@ -16,8 +16,24 @@ public class ReelOfPower : MonoBehaviour {
 
     public float positionInReel = 0;
 
-    public float MotionRate = 5;
+    public float RateForStarting = 15f;
+    float rateTurnDownTime;
+    public float MotionRate = 0f;
+
+    public bool stopped = true;
     // Use this for initialization
+    public void Action()
+    {
+        stopped = false;
+        MotionRate = RateForStarting;
+
+        ReelOfPower[] reels = transform.parent.GetComponentsInChildren<ReelOfPower>();
+        foreach (ReelOfPower reel in reels)
+        {
+            reel.MotionRate = reel.RateForStarting;
+        }
+    }
+
     void Start ()
     {
         // take the abilities and make up a reel of them according to weights.
@@ -66,15 +82,21 @@ public class ReelOfPower : MonoBehaviour {
 
         startPosition = transform.position.y;
         offset = images[1].transform.position.y- images[0].transform.position.y;
+        faction_OnePercentValue = (startPosition + offset) - startPosition;
     }
-	
+    public float faction_OnePercentValue = 0;
+    public float factionPercent = 0;
 	// Update is called once per frame
 	void Update ()
     {
+        //rateTurnDownTime = MotionRate;
+        //rateTurnDownTime = (rateTurnDownTime) % RateForStarting;
         positionInReel += MotionRate * Time.deltaTime;
 
         //if (Input.GetKey(KeyCode.K))
         //    positionInReel -= 4*Time.deltaTime;
+
+
 
         int startIndex = (int)positionInReel;
 
@@ -88,7 +110,45 @@ public class ReelOfPower : MonoBehaviour {
         }
 
         Vector3 pos = transform.position;
+        if (MotionRate == 0)
+        {
+            Debug.Log("Return Faction: " + fraction);
+            // keep moving slowly until we snap
+            if (fraction > 0.01f)
+                positionInReel += 0.1f * Time.deltaTime;
+
+            //if(pos.y > startPosition + 0.01f /*|| pos.y < startPosition - 0.1f*/ /*&& fraction < 1.1f*/)
+            //{
+                //float customRate
+            //    positionInReel = Mathf.Lerp(positionInReel, 0f, 1f);
+            //}
+
+            //if(fraction )
+
+            //if (transform.position.y <= startPosition - 0.5f && transform.position.y >= startPosition + 0.5f || transform.position.y <= startPosition + offset && transform.position.y >= startPosition - offset)
+            //{
+            //    if (transform.position.y > startPosition + (offset / 1.9))
+            //    {
+            //        //pos.y = startPosition + offset;
+            //        pos = Vector3.Lerp(pos, new Vector3(pos.x, startPosition + offset, pos.z), 0.1f);
+            //    }
+            //    else
+            //    {
+            //        pos = Vector3.Lerp(pos, new Vector3(pos.x, startPosition, pos.z), 0.1f);
+            //    }
+            //}
+        }
+        else
+        {
+            //slow it down over time
+            MotionRate *= 0.98f;
+            if (MotionRate < 0.1f)
+            {
+                MotionRate = 0;
+            }
+        }
         pos.y = startPosition - offset * fraction;
         transform.position = pos;
 	}
 }
+ 
