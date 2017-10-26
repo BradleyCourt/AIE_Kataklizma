9ebe97bf9d17f5ded86cec1fb5847e9c1cd10f44
Xaunaught <riley5694@gmail.com>
2017-10-26 14:43:22 +1030

    UI changes

---
+++ Assets/Scripts/Canvas/ActiveGame/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+    public GameObject pauseHolder;
+    public GameObject pauseSprite;
+    bool isPaused = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("p"))
+        {
+            if (!isPaused)
+            {
+                isPaused = true;
+                LeanTween.moveX(pauseHolder.GetComponent<RectTransform>(), -100, 1);
+                pauseSprite.SetActive(true);
+            }
+
+            else
+            {
+                isPaused = false;
+                LeanTween.moveX(pauseHolder.GetComponent<RectTransform>(), 102.5f, 1);
+                pauseSprite.SetActive(false);
+            }
+        }
+
+    }
+
+}
