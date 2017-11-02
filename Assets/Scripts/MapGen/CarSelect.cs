4a5f0739c6c898478b1a5aaddd2a0976744893af
Xaunaught <riley5694@gmail.com>
2017-11-02 11:26:15 +1030

    redid most of the prefabs

---
+++ Assets/Scripts/MapGen/CarSelect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSelect : MonoBehaviour {
+
+    public GameObject[] cars;
+	// Use this for initialization
+	void Start () {
+        Instantiate(cars[Random.Range(0, cars.Length)], transform.position, transform.rotation);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
