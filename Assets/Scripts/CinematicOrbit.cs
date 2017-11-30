30555be0291d74d2bc3e43cbe11530de6dcf84f5
Xaunaught <riley5694@gmail.com>
2017-11-30 15:22:28 +1030

    Cinematic Orbit

---
+++ Assets/Scripts/CinematicOrbit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicOrbit : MonoBehaviour {
+    public Kataklizma.Gameplay.PlayerController Controller;
+	// Use this for initialization
+	void Start () {
+        Controller.OrbitCamera = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
