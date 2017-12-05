8a5979ab62b8df9777260ab17bcf945ea0cd5e5f
Riley <riley5694@gmail.com>
2017-12-05 13:57:31 +1030

    Redid most of the sounds

---
+++ Assets/DetachOnStart.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetachOnStart : MonoBehaviour {
+
+    public bool LimitedLifespan = true;
+    public float TimeToLive = 2;
+
+    protected float Expiry;
+
+	// Use this for initialization
+	void Start () {
+        Expiry = Time.time + TimeToLive;
+
+        transform.parent = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Time.time > Expiry)
+            Destroy(gameObject);
+	}
+}
